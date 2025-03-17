676e6affb27be39c382e5641338ec332ab86ddfa
G R <gabi.romagnoli@kellogg.northwestern.edu>
2025-03-17 03:34:02 +0000

    prep

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["first_name"] = params["first_name"]
+    @user["first_name"] = params["first_name"] #TO DO: currently this is an issue / error. not sure why. Maybe need to change to username? 
     @user["last_name"] = params["last_name"]
     @user["email"] = params["email"]
     @user["password"] = params["password"]
     @user.save
-    redirect_to "/"
+    redirect_to "/users/#{@user["id"]}" #TO DO: something here. CHANGE THIS LATER
   end
 end
