32ab5226a6d3f89593f878d934e99c590ee36292
G R <gabi.romagnoli@kellogg.northwestern.edu>
2025-03-17 03:24:43 +0000

    getting ready

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,4 +8,13 @@ class SessionsController < ApplicationController
   def destroy
   end
 end
+
+# In the sessions controller, authenticate the user by email and password. 
+#  If successful, "log them in" (i.e. assign the user's id to the session hash) and redirect to the PLACES index. 
+#  If unsuccessful, redirect back to LOGIN.
+
+# Query for the logged-in user to conditionally show the login/signup links or a logout link (using the user's id in the session hash).
+
+# In the sessions controller, "logout" a logged-in user (i.e. remove the user's id from the session hash) and redirect to login.
+
   
